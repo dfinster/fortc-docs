97e6acc942e752877782982df7719a3d2c2c6709
David Finster <david@fortc.com>
2025-05-27 07:52:35 -0400

    Init the site with some basic docs and assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,76 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import catppuccin from "@catppuccin/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "David's Projects",
+      favicon: "favicon.ico",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/dfinster",
+        },
+      ],
+      head: [
+        {
+          tag: "link",
+          attrs: {
+            rel: "apple-touch-icon",
+            sizes: "180x180",
+            href: "/apple-touch-icon.png",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            href: "/favicon-32x32.png",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            href: "/favicon-16x16.png",
+          },
+        },
+        {
+          tag: "link",
+          attrs: {
+            rel: "manifest",
+            href: "/site.webmanifest",
+          },
+        },
+      ],
+      plugins: [
+        catppuccin({
+          dark: { flavor: "macchiato", accent: "mauve" },
+          light: { flavor: "latte", accent: "mauve" },
+        }),
+      ],
+      sidebar: [
+        {
+          label: "Dotfiles",
+          autogenerate: { directory: "dotfiles" },
+        },
+        {
+          label: "mkwww",
+          autogenerate: { directory: "mkwww" },
+        },
+        {
+          label: "clean-branches",
+          autogenerate: { directory: "clean-branches" },
+        },
+      ],
+    }),
+  ],
 });
